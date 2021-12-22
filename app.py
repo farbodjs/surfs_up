e8d188de62b72ee6c51791436265fcacc7e2fc11
Farbod_Jambor <farbodjambor@gmail.com>
2021-12-22 02:46:45 -0800

    changes

--- app.py
+++ app.py
@@ -1,17 +1,43 @@
-# import flask
+# import flask and dependencies
 from flask import Flask
+from flask import Flask, jsonify
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 # Create an app, being sure to pass_Name_
 app = Flask(__name__)
-# define what to do when a user gets to the index route
-@app.route('/')
-def home():
-    print("Server received request for 'Home' page ...")
-    return "Welcome to my 'Home' page!"
 
-# Define what to do when a user goes to the / about route
-@app.route("/about")
-def about():
-    print("Server received requests for 'About' page ...")
-    return "Welcome to my 'About' page!"
+
+    
+engine = create_engine("sqlite:///hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+session = Session(engine)
+app = Flask(__name__)
+import app
+
+print("example __name__ = %s", __name__)
+
+if __name__ == "__main__":
+    print("example is being run directly.")
+else:
+    print("example is being imported")
+@app.route("/")
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
 if __name__ == "__main__":
     app.run(dubug=True)
